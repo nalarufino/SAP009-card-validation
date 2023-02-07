05bb3f0a754a08e7dc2041144203180e7df9c67c
nalarufino <nalarufino@gmail.com>
2023-02-07 14:46:48 -0300

    Projeto parcialmente pronto, validação funcionando e maskify feito. Não está mostrando ainda o resultado do maskify mas já tem o espaço pra isso, falta fazer funcionar. EStilização pronta e html definido.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,80 @@
 import validator from './validator.js';
 
-console.log(validator);
+const form = document.querySelector("#credit-card");
+
+const cardNumber = document.querySelector("#card-number");
+const cardHolder = document.querySelector("#name-text");
+const cardExpiration = document.querySelector("#valid-thru-text");
+const cardCVV = document.querySelector("#cvv-text");
+
+const cardNumberText = document.querySelector(".number-vl");
+const cardHolderText = document.querySelector(".name-vl");
+const cardExpirationText = document.querySelector(".expiration-vl");
+const cardCVVText = document.querySelector(".cvv-vl");
+const button = document.getElementById("button")
+
+cardNumber.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+    cardNumberText.innerText = "#### #### #### 0000";
+    } else {
+        cardNumberText.innerHTML = e.target.value.replace("0000", masked());
+    }
+
+})
+
+cardHolder.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardHolderText.innerHTML = "TITULAR";
+    } else {
+        cardHolderText.innerHTML = e.target.value.toUpperCase();
+    }
+})
+
+cardExpiration.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardExpirationText.innerHTML = "mm/aa";
+    } else {
+        const valuesOfInput = e.target.value.replace("/", "");
+
+        if (e.target.value.length > 2) {
+            e.target.value = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+            cardExpirationText.innerHTML = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+        } else {
+            cardExpirationText.innerHTML = valuesOfInput;
+        }
+    }
+})
+
+cardCVV.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardCVVText.innerHTML = "123";
+    } else {
+        cardCVVText.innerHTML = e.target.value;
+    }
+})
+
+button.addEventListener("click", validation)
+
+function validation(e){
+    let digitValue = cardNumber.value
+    let valid = validator.isValid(digitValue)
+        if (digitValue === ""||
+        digitValue === "0000000000000000" ||
+        digitValue === "000000000000000"||
+        digitValue === "00000000000000" ||
+        digitValue.length <= 13
+        ) {
+            alert("Por favor, digite números válidos")
+        }
+        else if (valid === true) {
+            alert("Cadastro realizado com sucesso")
+            
+        } else if (!valid) {
+            alert("Número de cartão inválido")
+            
+        }
+
+}
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,23 @@
-const validator = {
-  // ...
-};
 
+const validator = {
+  isValid(creditCardNumber){
+    const arr = (creditCardNumber + '').split('').reverse().map(x => parseInt(x));
+    const lastDigit = arr.splice(0, 1)[0];
+    let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
+    sum += lastDigit;
+    return sum % 10 === 0;
+  },
+
+  maskify(creditCardNumber) {
+    const maskNum = [];
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if (i >= creditCardNumber.length - 4) {
+        maskNum.push(creditCardNumber[i]);
+      }
+    }
+    const masked = maskNum.join(""); //O método join() junta todos os elementos de um array (ou um array-like object) em uma string e retorna esta string
+    return masked;
+  },
+}
 export default validator;
+
